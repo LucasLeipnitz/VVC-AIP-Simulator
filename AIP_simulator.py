58b82a729c8eac955d81a327fe1b776368bbe248
Lucas Fraga <lucas.leipnitz.f@hotmail.com>
2023-06-26 18:55:30 -0300

    Transform_block file and coefficients dictionary
    Created transform_block file and coefficients dictionary

--- AIP_simulator.py
+++ AIP_simulator.py
@@ -3,242 +3,9 @@ import pandas as pd
 import math as mh
 import re
 from collections import defaultdict
+from transform_block import TransformBlock
 
 path = "C:/Users/Lucas/Desktop/Estudos/Bolsa/VVC_AIP_Simulator/"
-
-class TransformBlock:
-    '''Attributes:
-        int nTbW
-        int nTbH
-        int predModeIntra
-        int intraPredAngle
-        int refidx
-        int refW
-        int refH
-        int cidx
-        int refFilterFlag
-        int{ref_index} ref
-        int[] ref_id
-        int[] array_ifact
-        int[] array_iidx
-    '''
-
-    
-    def __init__(self, nTbW, nTbH, predModeIntra, intraPredAngle, refidx = 0, refW = 0, refH = 0, cidx = 0):
-        #Initialize inputs
-        self.nTbW = nTbW
-        self.nTbH = nTbH
-        self.predModeIntra = predModeIntra
-        self.intraPredAngle = intraPredAngle
-        self.refidx = refidx
-        self.refW = refW
-        self.refH = refH
-        self.cidx = cidx
-        
-
-        #refFilterFlag hardcoded for now
-        self.refFilterFlag = 1
-
-        #Initialize reference as an empty list
-        self.ref = defaultdict(lambda: "Null")
-        self.ref_id = []
-
-        #Initialize ifact and iidx array as empty lists
-        self.array_ifact = []
-        self.array_iidx = []
-
-        self.equations = []
-        self.equations_reuse = []
-
-    def calculate_reference_sample_array_greather_equal_34(self):
-        index_x = 0
-        index_y = - 1 - self.refidx
-
-        #with x = 0...nTbW + refidx + 1. The +1 on the end is to include (nTbW + refidx + 1) in the array
-        for x in range((self.nTbW + self.refidx + 1) + 1):
-            #ref[x] = p[-1 -refidx + x][-1 -refidx] 
-            index_x = -1 - self.refidx + x
-            self.ref[x] = "p[" + str(index_x) + "][" + str(index_y) + "]"
-            self.ref_id.append(x)
-
-        if (self.intraPredAngle < 0):
-            invAngle = round((512*32)/self.intraPredAngle)                
-            index_x = - 1 - self.refidx
-            index_y = 0
-
-            #with x = -nTbH ... -1
-            for x in range(-self.nTbH, 0):
-                #ref[x] = p[-1 -refidx][-1 -refidx + Min((x*invAngle + 256) >> 9, nTbH)]
-                index_y = -1 - self.refidx + min((x*invAngle + 256) >> 9, self.nTbH)
-                self.ref[x] = "p[" + str(index_x) + "][" + str(index_y) + "]"
-                self.ref_id.append(x)
-                    
-        else:
-            index_y = - 1 - self.refidx
-            #with x = nTbW + 2 + refidx ... refW + refidx
-            for x in range(self.nTbW + 2 + self.refidx, (self.refW + self.refidx) + 1):
-                #ref[x] = p[-1 -refidx + x][-1 -refidx]
-                index_x = -1 - self.refidx + x
-                self.ref[x] = ("p[" + str(index_x) + "][" + str(index_y) + "]")
-                self.ref_id.append(x)
-            
-            index_x = -1 + self.refW
-            #with x = 1...(Max(1,nTbW/nTbH)*refidx + 1)
-            for x in range(1,(max(1,self.nTbW/self.nTbH)*self.refidx + 1) + 1):
-                #ref[refW + refidx + x] = p[-1 -refW][-1 -refidx]
-                self.ref[self.refW + self.refidx + x] = "p[" + str(index_x) + "][" + str(index_y) + "]"
-                self.ref_id.append(x)
-
-        self.ref_id.sort()
-
-    def calculate_reference_sample_array_less_34(self):
-        index_x = - 1 - self.refidx
-        index_y = 0
-
-        #with x = 0...nTbH + refidx + 1. The +1 on the end is to include (nTbH + refidx + 1) in the array
-        for x in range((self.nTbH + self.refidx + 1) + 1):
-            #ref[x] = p[-1 -refidx][-1 -refidx + x] 
-            index_y = -1 - self.refidx + x
-            self.ref[x] = "p[" + str(index_x) + "][" + str(index_y) + "]"
-            self.ref_id.append(x)
-
-        if (self.intraPredAngle < 0):
-            invAngle = round((512*32)/self.intraPredAngle)                
-            index_x = 0
-            index_y = - 1 - self.refidx
-
-            #with x = -nTbW ... -1
-            for x in range(-self.nTbW, 0):
-                #ref[x] = p[-1 -refidx + Min((x*invAngle + 256) >> 9, nTbW][-1 -refidx]
-                index_x = -1 - self.refidx + min((x*invAngle + 256) >> 9, self.nTbH)
-                self.ref[x] = "p[" + str(index_x) + "][" + str(index_y) + "]"
-                self.ref_id.append(x)
-                    
-        else:
-            index_x = - 1 - self.refidx
-            #with x = nTbH + 2 + refidx ... refH + refidx
-            for x in range(self.nTbH + 2 + self.refidx, (self.refH + self.refidx) + 1):
-                #ref[x] = p[-1 -refidx][-1 -refidx + x]
-                index_y = -1 - self.refidx + x
-                self.ref[x] = ("p[" + str(index_x) + "][" + str(index_y) + "]")
-                self.ref_id.append(x)
-            
-            index_y = -1 + self.refH
-            #with x = 1...(Max(1,nTbH/nTbW)*refidx + 1)
-            for x in range(1,(max(1,self.nTbH/self.nTbW)*self.refidx + 1) + 1):
-                #ref[refH + refidx + x] = p[-1 -refidx][-1 -refH]
-                self.ref[self.refH + self.refidx + x] = "p[" + str(index_x) + "][" + str(index_y) + "]"
-                self.ref_id.append(x)
-
-        self.ref_id.sort()
-
-
-    def calculate_pred_values(self):
-        if (self.predModeIntra >= 34):
-            self.calculate_reference_sample_array_greather_equal_34()
-        else:
-            self.calculate_reference_sample_array_less_34()
-            pass
-            
-
-    def calculate_constants_mode(self):
-        for x in range(self.nTbW):
-            iidx = ((x + 1)*self.intraPredAngle) >> 5
-            ifact = ((x + 1)*self.intraPredAngle) & 31
-            #print("When x = " + str(x) + ", f = ((" + str(x) + " + 1)* " + str(angle) + ") & 31"," = ",ifact)
-            self.array_iidx.append(iidx)
-            self.array_ifact.append(ifact)
-
-    def calculate_equations_mode(self):
-        equations = []
-        columns = []
-        for x in range(self.nTbW): 
-            columns.append(x)
-            current_column = []
-            iidx = ((x + 1)*self.intraPredAngle) >> 5
-            ifact = ((x + 1)*self.intraPredAngle) & 31
-            for y in range(self.nTbH):
-                current_column.append("fC[" + str(ifact) + "][0]*ref[" + str(y + iidx + 0) + "] + " + "fC[" + str(ifact) +
-                                        "][1]*ref[" + str(y + iidx + 1) + "] + " + "fC[" + str(ifact) + "][2]*ref[" +
-                                        str(y + iidx + 2) + "] + " + "fC[" + str(ifact) + "][3]*ref[" + str(y + iidx + 3) + "]")        
-            self.equations.append(current_column)
-
-        df = pd.DataFrame(list(zip(*self.equations)),columns = columns)
-        excel_writer = pd.ExcelWriter(path + "equations_" + str(self.predModeIntra) + "_" + str(self.nTbW) + "x" + str(self.nTbH) + ".xlsx", engine='xlsxwriter') 
-        df.to_excel(excel_writer, sheet_name='equations', index=False, na_rep='NaN')
-
-        # Auto-adjust columns' width
-        for column in df:
-            column_width = 70
-            col_iidx = df.columns.get_loc(column)
-            excel_writer.sheets['equations'].set_column(col_iidx, col_iidx, column_width)
-
-        excel_writer._save()
-        return equations
-
-    '''def calculate_equation_with_reuse(self, buffer, x):
-        columns.append(x)
-        current_column = []
-        iidx = ((x + 1)*self.intraPredAngle) >> 5
-        ifact = ((x + 1)*self.intraPredAngle) & 31
-        for y in range(self.nTbH):
-            if(ifact in buffer):
-                if((y + iidx) in buffer[ifact]):
-                    current_column.append("reuso: " + str(buffer[ifact][y + iidx]))   
-                else:
-                    current_column.append("fC[" + str(ifact) + "][0]*ref[" + str(y + iidx + 0) + "] + " + "fC[" + str(ifact) +
-                                            "][1]*ref[" + str(y + iidx + 1) + "] + " + "fC[" + str(ifact) + "][2]*ref[" +
-                                            str(y + iidx + 2) + "] + " + "fC[" + str(ifact) + "][3]*ref[" + str(y + iidx + 3) + "]")
-                    buffer[ifact][y + iidx] = str(self.predModeIntra) + " : " + str(x)
-            else:
-                current_column.append("fC[" + str(ifact) + "][0]*ref[" + str(y + iidx + 0) + "] + " + "fC[" + str(ifact) +
-                                            "][1]*ref[" + str(y + iidx + 1) + "] + " + "fC[" + str(ifact) + "][2]*ref[" +
-                                            str(y + iidx + 2) + "] + " + "fC[" + str(ifact) + "][3]*ref[" + str(y + iidx + 3) + "]")
-                buffer[ifact][y + iidx] = str(self.predModeIntra) + " : " + str(x) + "," + str(y)
-
-            self.equations_reuse.append(current_column)'''
-    
-    
-    def print_reference_sample_array(self):
-        ref = []
-        for i in self.ref:
-            ref.append((i, self.ref[i]))
-
-        sorted_ref = sorted(ref)
-        for i in sorted_ref:
-            print(i)
-
-    def transform_dict_to_array(self, begin, end, normalize):
-        ref = []
-        for i in range(begin, end + 1):
-            last_iidx = (((self.nTbW - 1) + 1)*self.intraPredAngle) >> 5 #iidx da última posição de x ou y
-            if(last_iidx < 0):
-                if(not(normalize)):
-                    if(i < last_iidx):
-                        ref.append("NU")
-                    else:
-                        ref.append(self.ref[i])
-                else:
-                    ref.append(self.ref[i])
-            else:
-                if(not(normalize)):
-                    if(i > (last_iidx + (self.nTbW - 1) + 3)):
-                        ref.append("NU")
-                    else:
-                        ref.append(self.ref[i])
-                else:
-                    ref.append(self.ref[i])
-        return ref
-            
-
-    def normalize_ref(self):
-        for i in self.ref.keys():
-            x,y = re.findall(r'\d+', self.ref[i]) #Get x and y value from string
-            if(i < 0):
-                if(y != str(abs(i) - 1)):
-                    self.ref[int('-' + str(int(y) + 1))] = self.ref[i]
-                    self.ref[i] = 'NU'
-
 
 modes1 = [34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66]
 modes2 = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
@@ -250,6 +17,78 @@ angles2 = [32,29,26,23,20,18,16,14,12,10,8,6,4,3,2,1,0,-1,-2,-3,-4,-6,-8,-10,-12
 angles3 = [32,29,26,23,20,18,16,14,12,10,8,6,4,3,2,1,0,-1,-2,-3,-4,-6,-8,-10,-12,-14,-16,-18,-20,-23,-26,-29,-32]
 angles_positive = [0,1,2,3,4,6,8,10,12,14,16,18,20,23,26,29,32]
 angles_negative = [-32,-29,-26,-23,-20,-18,-16,-14,-12,-10,-8,-6,-4,-3,-2,-1,0]
+
+fc_coefficients = {
+    "0[0]" : 0, 
+    "1[0]" : -1,
+    "2[0]" : -2,
+    "3[0]" : -2,
+    "4[0]" : -2,
+    "5[0]" : -3,
+    "6[0]" : -4,
+    "7[0]" : -4,
+    "8[0]" : -4,
+    "9[0]" : -5,
+    "10[0]" : -6,
+    "11[0]" : -6,
+    "12[0]" : -6,
+    "13[0]" : -5,
+    "14[0]" : -4,
+    "15[0]" : -4,
+    "16[0]" : -4,
+    "17[0]" : -4,
+    "18[0]" : -4,
+    "19[0]" : -4,
+    "20[0]" : -4,
+    "21[0]" : -3,
+    "22[0]" : -2,
+    "23[0]" : -2,
+    "24[0]" : -2,
+    "25[0]" : -2,
+    "26[0]" : -2,
+    "27[0]" : -2,
+    "28[0]" : -2,
+    "29[0]" : -1,
+    "30[0]" : 0,
+    "31[0]" : 0,
+    "0[1]" : 64, 
+    "1[1]" : 63,
+    "2[1]" : 62,
+    "3[1]" : 60,
+    "4[1]" : 58,
+    "5[1]" : 57,
+    "6[1]" : 56,
+    "7[1]" : 55,
+    "8[1]" : 54,
+    "9[1]" : 53,
+    "10[1]" : 52,
+    "11[1]" : 49,
+    "12[1]" : 46,
+    "13[1]" : 44,
+    "14[1]" : 42,
+    "15[1]" : 39,
+    "16[1]" : 36,
+    "17[1]" : 33,
+    "18[1]" : 30,
+    "19[1]" : 29,
+    "20[1]" : 28,
+    "21[1]" : 24,
+    "22[1]" : 20,
+    "23[1]" : 18,
+    "24[1]" : 16,
+    "25[1]" : 15,
+    "26[1]" : 14,
+    "27[1]" : 12,
+    "28[1]" : 10,
+    "29[1]" : 7,
+    "30[1]" : 4,
+    "31[1]" : 2,
+    "0[2]" : 0,
+    "0[3]" : 0
+}
+
+def simmetry_rule(p, index):
+    return 32 - p, 3 - index
 
 def calculate_iidx_ifact(modes, angles, size):
     values_ifact = []
