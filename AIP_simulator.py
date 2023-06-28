22ef463794da28a60af9c060d297463c211644e7
Lucas De Fraga <lucas.leipnitz.f@hotmail.com>
2023-06-28 14:02:56 -0300

    Simulator update, bug corrections and new tests

--- AIP_simulator.py
+++ AIP_simulator.py
@@ -5,7 +5,7 @@ import re
 from collections import defaultdict
 from transform_block import TransformBlock
 
-path = "C:/Users/Lucas/Desktop/Estudos/Bolsa/VVC_AIP_Simulator/"
+path = "~/VVC-AIP-Simulator"
 
 modes1 = [34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66]
 modes2 = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
@@ -168,16 +168,16 @@ def calculate_states(modes, angles, block_size, state_size):
         base_counter = 0
         n_state_iidx = 0
         n_state_ifact = 0
-        state_iidx = ""
+        state_iidx = []
         state_ifact = []
         array_states_iidx = ["Null" for x in range(int(32/state_size))] #it has at most 32/state_size states, because at 32 it starts to repeat
         array_states_ifact = ["Null" for x in range(int(32/state_size))] #it has at most 32/state_size states, because at 32 it starts to repeat
         for iidx,ifact in zip(i,j):
             if(base == iidx):
-                state_iidx = state_iidx + str(base_counter)
+                state_iidx.append(base_counter)
             else:
                 base_counter = base_counter + 1
-                state_iidx = state_iidx + str(base_counter)
+                state_iidx.append(base_counter)
             base = iidx
             count = count + 1
             state_ifact.append(ifact)
@@ -187,7 +187,7 @@ def calculate_states(modes, angles, block_size, state_size):
                 else:
                     array_states_iidx[n_state_iidx] = state_iidx
                     n_state_iidx = n_state_iidx + 1
-                state_iidx = ""
+                state_iidx = []
                 base_counter = 0
                 
             if((count + 1)%state_size == 0):
@@ -208,13 +208,13 @@ def calculate_states(modes, angles, block_size, state_size):
     return df_iidx
     
 def calculate_MCM_blocks(mode, state_iidx, state_ifact, base = 0, height = 1, replicate = 0, print_values = 0):
-    downward_index = base #downward will begin from the base
     constants_vectors = {}
     variation = 0
     
     #Number of fases equals to the size of the block 
     
     #Initial fase
+    downward_index = base #downward will begin from the base
     for i,j in zip(state_iidx, range(len(state_iidx))):
         variation = int(i) - variation
         if(variation == 0):
@@ -223,32 +223,32 @@ def calculate_MCM_blocks(mode, state_iidx, state_ifact, base = 0, height = 1, re
             variation = int(i)
             if(mode >= 34):
                 if(mode >= 50):
-                    base = base + int(i)
+                    downward_index = base + int(i)
                 else:
-                    base = base - int(i)
+                    downward_index = base - int(i)
             else:
                 #TODO modes < 34
                 pass
         
         if(base not in constants_vectors):
-            constants_vectors[base] = []
+            constants_vectors[downward_index] = []
         
-        constants_vectors[base].append(str(state_ifact[j]) + "[0]")
+        constants_vectors[downward_index].append(str(state_ifact[j]) + "[0]")
         
-        if((base + 1) not in constants_vectors):
-            constants_vectors[base + 1] = []
+        if((downward_index + 1) not in constants_vectors):
+            constants_vectors[downward_index + 1] = []
 
-        constants_vectors[base + 1].append(str(state_ifact[j]) + "[1]")
+        constants_vectors[downward_index + 1].append(str(state_ifact[j]) + "[1]")
     
-        if((base + 2) not in constants_vectors):
-            constants_vectors[base + 2] = []
+        if((downward_index + 2) not in constants_vectors):
+            constants_vectors[downward_index + 2] = []
         
-        constants_vectors[base + 2].append(str(state_ifact[j]) + "[2]")
+        constants_vectors[downward_index + 2].append(str(state_ifact[j]) + "[2]")
     
-        if((base + 3) not in constants_vectors):
-            constants_vectors[base + 3] = []
+        if((downward_index + 3) not in constants_vectors):
+            constants_vectors[downward_index + 3] = []
         
-        constants_vectors[base + 3].append(str(state_ifact[j]) + "[3]")
+        constants_vectors[downward_index + 3].append(str(state_ifact[j]) + "[3]")
         
     if(height == 1):
         pass #dont need to calculate other lines, only the first fase is necessary
@@ -259,9 +259,9 @@ def calculate_MCM_blocks(mode, state_iidx, state_ifact, base = 0, height = 1, re
 
         if(mode >= 34):
             if(mode >= 50):
-                pass #downward will begin from the base
-            else: #negative iidx values only exists to modes < 50
-                downward_index = base #downward will begin from the lowest of the negative values
+                downward_index = base #downward will begin from the base
+            else: #negative iidx values only exists on modes < 50
+                pass #downward will begin from the lowest of the negative values
         else:
             #TODO modes < 34
             pass
@@ -324,7 +324,7 @@ def merge_states(constants_or_coefficients_list, replicate = 0, print_values = 0
 def calculate_adders(state_iidx, state_ifact, base = 0):
     pass
 
-#calculate_states(modes1, angles1, 64, 8)
+#calculate_states(modes1, angles1, 64, 32)
 #calculate_MCM_blocks(56,"0001",[8,16,24,0])
 #calculate_MCM_blocks(56,"0001",[8,16,24,0], 1)
 #calculate_MCM_blocks(44,"1000",[24,16,8,0])
@@ -343,13 +343,26 @@ coef.append(map_to_coefficients(calculate_MCM_blocks(35,"11111011",[19, 22, 25, 
 coef.append(map_to_coefficients(calculate_MCM_blocks(35,"11111110",[11, 14, 17, 20, 23, 26, 29, 0]), fc_coefficients))'''
 
 #merge_states(coef, print_values = 1)
-calculate_MCM_blocks(59,"00101010",[14, 28, 10, 24, 6, 20, 2, 16], height = 1, print_values = 1)    
-calculate_MCM_blocks(59,"01010101",[30, 12, 26, 8, 22, 4, 18, 0], height = 1, base = 3, print_values = 1)
-calculate_MCM_blocks(59,"00101010",[14, 28, 10, 24, 6, 20, 2, 16], height = 1, base = 7, print_values = 1)
-calculate_MCM_blocks(59,"01010101",[30, 12, 26, 8, 22, 4, 18, 0], height = 1, base = 10, print_values = 1)              
-                
-        
+
+'''calculate_MCM_blocks(59,"00112233",[14, 28, 10, 24, 6, 20, 2, 16], height = 1, print_values = 1)
+calculate_MCM_blocks(59,"01122334",[30, 12, 26, 8, 22, 4, 18, 0], height = 1, base = 3, print_values = 1)
+calculate_MCM_blocks(59,"00112233",[14, 28, 10, 24, 6, 20, 2, 16], height = 1, base = 7, print_values = 1)
+calculate_MCM_blocks(59,"01122334",[30, 12, 26, 8, 22, 4, 18, 0], height = 1, base = 10, print_values = 1)'''
 
 
+#map_to_coefficients(calculate_MCM_blocks(59,"00112233",[14, 28, 10, 24, 6, 20, 2, 16], height = 1), fc_coefficients, print_values = 1)    
+#map_to_coefficients(calculate_MCM_blocks(59,"01122334",[30, 12, 26, 8, 22, 4, 18, 0], height = 1, base = 3,), fc_coefficients, print_values = 1)
+#map_to_coefficients(calculate_MCM_blocks(59,"00112233",[14, 28, 10, 24, 6, 20, 2, 16], height = 1, base = 7), fc_coefficients, print_values = 1)    
+#map_to_coefficients(calculate_MCM_blocks(59,"01122334",[30, 12, 26, 8, 22, 4, 18, 0], height = 1, base = 10), fc_coefficients, print_values = 1) 
+#map_to_coefficients(calculate_MCM_blocks(59,[0,0,1,1,2,2,3,3,3,4,4,5,5,6,6,7,7,7,8,8,9,9,10,10,10,11,11,12,12,13,13,14],[14, 28, 10, 24, 6, 20, 2, 16, 30, 12, 26, 8, 22, 4, 18, 0, 14, 28, 10, 24, 6, 20, 2, 16, 30, 12, 26, 8, 22, 4, 18, 0], height = 1), fc_coefficients, print_values = 1)
+#calculate_MCM_blocks(59,[0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8, 9, 9, 10, 10, 10, 11, 11, 12, 12, 13, 13, 14],[14, 28, 10, 24, 6, 20, 2, 16, 30, 12, 26, 8, 22, 4, 18, 0, 14, 28, 10, 24, 6, 20, 2, 16, 30, 12, 26, 8, 22, 4, 18, 0], height = 1, print_values = 1) 
+#calculate_MCM_blocks(59,[0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8, 9, 9, 10, 10, 10, 11, 11, 12, 12, 13, 13, 14],[14, 28, 10, 24, 6, 20, 2, 16, 30, 12, 26, 8, 22, 4, 18, 0, 14, 28, 10, 24, 6, 20, 2, 16, 30, 12, 26, 8, 22, 4, 18, 0], height = 1, print_values = 1)          
+#calculate_MCM_blocks(51,[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 0], height = 1, print_values = 1)        
+#map_to_coefficients(calculate_MCM_blocks(51,[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 0], height = 1),fc_coefficients ,print_values = 1)     
+
+#calculate_MCM_blocks(51,[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], height = 1, print_values = 1)
+#calculate_MCM_blocks(51,[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 0], height = 1, base = 0,print_values = 1)
+map_to_coefficients(calculate_MCM_blocks(51,[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], height = 1), fc_coefficients ,print_values = 1)
+map_to_coefficients(calculate_MCM_blocks(51,[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], height = 1), fc_coefficients ,print_values = 1)
 
 
